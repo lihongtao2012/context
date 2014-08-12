5da8a44bab23efb71e2cba67c803c24abcdbab13
lihongtao2014 <1004646858@qq.com>
2014-08-12 16:33:34 +0800

    lihongtao
    contextTEST

---
+++ AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  context
+//
+//  Created by MacPro.com on 14-8-12.
+//  Copyright (c) 2014年 lihongtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
