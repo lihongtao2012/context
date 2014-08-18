bc49ff86eec722ca5407d3c54378470019c21839
lihongtao2014 <1004646858@qq.com>
2014-08-18 22:35:56 +0800

    lihongtao
    lihongtao

---
+++ ViewControlls/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  context
+//
+//  Created by MacPro.com on 14-8-12.
+//  Copyright (c) 2014年 lihongtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ ViewControlls/FirstViewController.m
@@ -0,0 +1,54 @@
+//
+//  FirstViewController.m
+//  context
+//
+//  Created by MacPro.com on 14-8-12.
+//  Copyright (c) 2014年 lihongtao. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@property (strong, nonatomic) IBOutlet UILabel *aLbabel;
+
+@end
+
+@implementation FirstViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    
+    
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
